b82a7d1085c2b1104137c7346261b9c60e4a507c
BroWuGou <a924673816@outlook.com>
2021-07-03 11:07:05 +0800

    新增 MBP 的 Service CRUD 接口

--- ruoyi-usertool/src/main/java/com/ruoyi/usertool/service/impl/StationServiceImpl.java
+++ ruoyi-usertool/src/main/java/com/ruoyi/usertool/service/impl/StationServiceImpl.java
@@ -1,9 +1,13 @@
 package com.ruoyi.usertool.service.impl;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.ruoyi.common.annotation.Log;
+import com.ruoyi.usertool.service.StationService;
 import org.apache.shiro.crypto.hash.Hash;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,11 +23,14 @@ import com.ruoyi.common.core.text.Convert;
  * @date 2021-07-02
  */
 @Service
-public class StationServiceImpl implements IStationService 
+public class StationServiceImpl extends ServiceImpl<StationMapper, Station>
+        implements IStationService, StationService
 {
     @Autowired
     private StationMapper stationMapper;
 
+    @Autowired
+    private StationService stationService;
     /**
      * 查询站点
      * 
@@ -63,7 +70,18 @@ public class StationServiceImpl implements IStationService
      * @return 结果
      */
     @Override
-    public int insertStation(Station station) { return stationMapper.insert(station); }
+    public int insertStation(Station station) {
+//        //检查表中是否存在主键一致的数据
+//        QueryWrapper<Station> wrapper = new QueryWrapper<>();
+//        if (station.getStationName() != null) {
+//            wrapper.eq("station_name", station.getStationName());
+//        }
+//
+//        boolean ret = stationService.saveOrUpdate(station, wrapper);
+//        // 如果和被标记删除的数据发生主键冲突，则改为修改到原数据上
+        // TODO: 逻辑删除的数据的station_name会与新插入数据重名，导致插入失败，目前暂时将表中的unique去掉，需要在在代码层增加unique实现
+        return stationMapper.insert(station);
+    }
 
     /**
      * 修改站点
@@ -83,8 +101,9 @@ public class StationServiceImpl implements IStationService
     @Override
     public int deleteStationByIds(String ids)
     {
-        return 0;
-//        return stationMapper.deleteStationByIds(Convert.toStrArray(ids));
+        String[] idsStr = Convert.toStrArray(ids);
+        List<String> strings = Arrays.asList(idsStr);
+        return stationMapper.deleteBatchIds(strings);
     }
 
     /**
